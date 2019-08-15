37a4b99a942c88c3eea4c358004bd662c6d8b2e2
daniel.mwangila <margalight@outlook.com>
2019-08-15 16:29:50 +0200

    added initial tag features for v0.0.1

--- example/src/App.js
+++ example/src/App.js
@@ -1,22 +1,93 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import Tagify from 'react-tagify-section'
+import Tagify from 'react-tagify-section';
+import './styles.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tags: null
+    };
+  }
   tags = [
     {
-      name: 'java'
-    },
-    {
-      name: 'javascript'
-    },
-    {
       name: 'laravel'
+    },
+    {
+      name: 'C',
+      year: 1972
+    },
+    {
+      name: 'C#',
+      year: 2000
+    },
+    {
+      name: 'C++',
+      year: 1983
+    },
+    {
+      name: 'Clojure',
+      year: 2007
+    },
+    {
+      name: 'Elm',
+      year: 2012
+    },
+    {
+      name: 'Go',
+      year: 2009
+    },
+    {
+      name: 'Haskell',
+      year: 1990
+    },
+    {
+      name: 'Java',
+      year: 1995
+    },
+    {
+      name: 'Javascript',
+      year: 1995
+    },
+    {
+      name: 'Perl',
+      year: 1987
+    },
+    {
+      name: 'PHP',
+      year: 1995
+    },
+    {
+      name: 'Python',
+      year: 1991
+    },
+    {
+      name: 'Ruby',
+      year: 1995
+    },
+    {
+      name: 'Scala',
+      year: 2003
     }
   ];
+
+  listofTags = value => {
+    this.setState({ tags: value });
+    console.log(this.state);
+  };
   render() {
-    return <Tagify tags={this.tags} />
+    console.log(this.state);
+
+    return (
+      <div className='container'>
+        <div className='form-group'>
+          <label htmlFor='tags'>Tags</label>
+          <Tagify tags={this.tags} getTagList={this.listofTags} />
+        </div>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-unused-expressions */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
 
-import styles from './styles.css'
+import './styles.css'
 
 export default class Tagify extends Component {
   constructor() {
@@ -11,7 +12,8 @@ export default class Tagify extends Component {
     this.initialId = 0
   }
   static propTypes = {
-    tags: PropTypes.arrayOf(Object).isRequired
+    tags: PropTypes.arrayOf(Object).isRequired,
+    getTagList: PropTypes.func.isRequired
   };
 
   componentDidMount() {
@@ -19,35 +21,37 @@ export default class Tagify extends Component {
     this.appendNewTag(tags)
   }
 
+  escapeRegexCharacters = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
   getSuggestions = value => {
+    const escapedValue = this.escapeRegexCharacters(value.trim())
     const { tags } = this.props
-    const inputValue = value.trim().toLowerCase()
-    const inputLength = inputValue.length
 
-    return inputLength === 0
-      ? []
-      : tags.filter(
-        word => word.name.toLowerCase().slice(0, inputLength) === inputValue
-      )
+    if (escapedValue === '') {
+      return []
+    }
+
+    const regex = new RegExp('^' + escapedValue, 'i')
+    const suggestions = tags.filter(tag => regex.test(tag.name))
+
+    return suggestions
   };
 
   appendNewTag = () => {
-    const { tags } = this.props
     // auto suggestion wrapper
     const autoSuggestionWrapper = document.createElement('div')
     autoSuggestionWrapper.setAttribute('id', `sug-${this.initialId}`)
     autoSuggestionWrapper.classList.add('tags__autosuggest')
 
     // tags container
-    const tag = document.getElementById('tags')
+    const tag = document.getElementById('tagsWrapper')
     const tagElement = document.createElement('input')
     tagElement.classList.add('tags__inputs')
-    tagElement.setAttribute('id', 'done')
+    tagElement.setAttribute('id', 'inputWrapper')
     tag.appendChild(autoSuggestionWrapper)
     autoSuggestionWrapper.appendChild(tagElement)
 
     // suggestion box
-    const done = document.getElementById('done')
+    const done = document.getElementById('inputWrapper')
     const suggestionBox = document.createElement('ul')
     suggestionBox.classList.add('tags__suggestion')
     suggestionBox.setAttribute('id', 'suggestion-box')
@@ -64,24 +68,23 @@ export default class Tagify extends Component {
         _.indexOf(this.listOfTags, delTag.previousSibling.id, 0),
         1
       )
-      this.articleContentUpdater(this.props, { tags: this.listOfTags })
       const elem = document.getElementById(delTag.parentNode.id)
       elem.parentNode.removeChild(elem)
-    };
+    }
 
     done.addEventListener('keyup', key => {
       if (key) {
         const words = this.getSuggestions(key.target.value)
         let tagNames
-        let list = 0
+
+        const elem = document.getElementById('suggestion-box')
+        if (elem) {
+          elem.innerHTML = ''
+        }
         if (words.length !== 0) {
-          while (suggestionBox.childNodes.length > 0) {
-            suggestionBox.removeChild(suggestionBox.childNodes[list])
-            list++
-          }
           words.map(word => {
             tagNames = document.createElement('li')
-            tagNames.setAttribute('id', 'tag-index')
+            tagNames.setAttribute('id', `tag-index`)
             const names = document.createTextNode(word.name)
             tagNames.appendChild(names)
             tagNames.onclick = event => {
@@ -103,24 +106,23 @@ export default class Tagify extends Component {
                 done.removeAttribute('id')
                 done.setAttribute('id', word.name)
                 this.listOfTags.push(key.target.value.trim())
+                this.props.getTagList(this.listOfTags)
                 this.appendNewTag()
               }
             }
+            suggestionBox.appendChild(tagNames)
+            autoSuggestionWrapper.appendChild(suggestionBox)
           })
-
-          suggestionBox.appendChild(tagNames)
-
-          autoSuggestionWrapper.appendChild(suggestionBox)
         } else {
           const sugBox = document.getElementById('suggestion-box')
           sugBox ? autoSuggestionWrapper.removeChild(sugBox) : ''
         }
       }
-      if (key.which === 32 || key.which === 9) {
+      if (key.which === 13) {
         const sugBox = document.getElementById('suggestion-box')
         sugBox ? autoSuggestionWrapper.removeChild(sugBox) : ''
         done.removeAttribute('id')
-        done.setAttribute('id', event.target.value.trim())
+        done.setAttribute('id', key.target.value.trim())
 
         if (_.indexOf(this.listOfTags, key.target.value, 0) > -1) {
           const originalTag = document.getElementById(key.target.value.trim())
@@ -134,6 +136,7 @@ export default class Tagify extends Component {
           done.disabled = true
           autoSuggestionWrapper.appendChild(deleteIcon)
           this.listOfTags.push(key.target.value.trim())
+          this.props.getTagList(this.listOfTags)
           this.appendNewTag()
         }
       }
@@ -141,6 +144,6 @@ export default class Tagify extends Component {
   };
 
   render() {
-    return <div className={styles.tags} id='tags' />
+    return <div className='tags' id='tagsWrapper' />
   }
 }
